e66f831c1da6019df385e06ccd1e4d39ea83857e
dcbounds <boundsdarius@gmail.com>
2019-07-09 00:50:14 +0000

    start

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -22,11 +22,21 @@ const injectContext = PassedComponent => {
 		}
 
 		componentDidMount() {
-			/**
-			 * EDIT THIS!
-			 * This function is the equivalent to "window.onLoad", it only run once on the entire application lifetime
-			 * you should do your ajax requests or fetch api requests here
-			 **/
+			fetch("./api/some.json")
+				.then(function(response) {
+					if (response.status !== 200) {
+						console.log("Looks like there was a problem. Status Code: " + response.status);
+						return;
+					}
+
+					// Examine the text in the response
+					response.json().then(function(data) {
+						console.log(data);
+					});
+				})
+				.catch(function(err) {
+					console.log("Fetch Error :-S", err);
+				});
 		}
 
 		render() {
